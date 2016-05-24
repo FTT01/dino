f1da40859cd4a49502cc5db1de35f27ebab4baa6
FTT01 <dallatorre.daniele@gmail.com>
2016-05-24 15:59:09 +0200

    Added test file

---
+++ KinematicWave.java
@@ -0,0 +1,32 @@
+/*
+ * GNU GPL v3 License
+ *
+ * Copyright 2015 AboutHydrology (Riccardo Rigon)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package it.blogspot.geoframe;
+
+/**
+ * @brief This components computes the propagation of the Kinematic Wave
+ *
+ * @author sidereus, francesco.serafin.3@gmail.com
+ * @author ftt01, dallatorre.daniele@gmail.com
+ * @version 0.1
+ * @date May 15, 2015
+ * @copyright GNU Public License v3 GWH-2b4 (GEOframe developers)
+ */
+public class KinematicWave {
+
+}
